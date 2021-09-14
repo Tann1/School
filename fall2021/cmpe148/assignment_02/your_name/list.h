8f66354ec3fa0852eb81eb2d1c23c10973ee17b4
Tann1 <gadkaritanishq@gmail.com>
2021-09-14 15:47:13 -0700

    added fall2021

---
+++ fall2021/cmpe148/assignment_02/your_name/list.h
@@ -0,0 +1,40 @@
+#ifndef LIST_H
+#define LIST_H
+
+static inline struct linked_list *
+ll_create(void)
+{
+	return NULL;
+}
+
+static inline int
+ll_destroy(struct linked_list *ll)
+{
+
+}
+
+static inline void
+ll_add(struct linked_list *ll, int value)
+{
+
+}
+
+static inline int
+ll_length(struct linked_list *ll)
+{
+	return 0;
+}
+
+static inline bool
+ll_remove_first(struct linked_list *ll)
+{
+	return NULL;
+}
+
+static inline int
+ll_contains(struct linked_list *ll, int value)
+{
+	return 0;
+}
+
+#endif
