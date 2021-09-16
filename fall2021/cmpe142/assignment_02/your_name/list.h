4ed2e5aa767409e34a78f2e9f5dad8c452c269fe
Tann1 <gadkaritanishq@gmail.com>
2021-09-16 15:33:26 -0700

    list header file works fine for now

--- fall2021/cmpe142/assignment_02/your_name/list.h
+++ fall2021/cmpe142/assignment_02/your_name/list.h
@@ -8,56 +8,62 @@
 #include <stdio.h>
 
 
-struct linked_list {
-	double val;
-	struct linked_list* next;
+struct Node {
+	int val;
+	struct Node* next;
 };
+
+struct linked_list {
+	struct Node* head; 
+};
+
 
 static inline struct linked_list *
 ll_create(void)
 {
-	struct linked_list* ll = (struct linked_list *) malloc(sizeof(struct linked_list)); //returns NULL if it can't allocate the desired memory space 
-	ll->val = HUGE_VAL; 						       //indicates that the node is empty 
-	return ll;
+	return (struct linked_list *) malloc(sizeof(struct linked_list)); //returns NULL if it can't allocate the desired memory space 
 }
 
-/*
+
 static inline int
 ll_destroy(struct linked_list *ll)
 {
-
+	if (ll != NULL && ll->head == NULL) {
+		free(ll);
+		return 1;
+	}
+	return 0;	
 }
-*/
+
 
 static inline void
 ll_add(struct linked_list *ll, int value)
 {
-	struct linked_list* temp;
+	struct Node* temp;
 
 	if (ll == NULL) {			//if empty
 		printf("Linked List is empty must create list first\n");
 		return;
 	}
-	if (ll->val == HUGE_VAL) {		//if the list is created but is still empty relative to its value 
-		ll->val = (double) value;
-		ll->next = NULL;
+	temp = (struct Node *) malloc(sizeof(struct Node));
+	temp->val = value;
+	temp->next = NULL;
+	if (ll->head == NULL) {		//if the list is created but is still empty 
+		ll->head = temp;
 		return;
 	}
-	temp = ll_create();
-	temp->val = (double) value;
-	temp->next = NULL;
-	temp->next = ll;			//let current head become temp's next so temp becomes current head
-	ll = temp;				//update ll to temp since temp is current head now 
+	
+	temp->next = ll->head;		//since temp will be the new head update the current head to be temp's next
+	ll->head = temp;		//now replace current head with temp (the new head)
 }
-
 static inline int
 ll_length(struct linked_list *ll)
 {
 	int length = 0;
-	
-	while (ll != NULL) {
+	struct Node* temp = ll->head;	
+	while (temp != NULL) {
 		length++;
-		ll = ll->next;
+		temp = temp->next;
 	}	
 	return length;
 }
@@ -65,12 +71,12 @@ ll_length(struct linked_list *ll)
 static inline bool
 ll_remove_first(struct linked_list *ll)
 {
-	struct linked_list* temp; 
+	struct Node* temp; 
 
-	if (ll == NULL || ll->val == HUGE_VAL)	//if empty
+	if (ll == NULL || ll->head == NULL)	//if empty
 		return false;		
-	temp = ll;				//capture the head in a temp variable
-	ll = ll->next;				//update the current head to be head's next node (the new head)
+	temp = ll->head;			//capture the head in a temp variable
+	ll->head = ll->head->next;		//update the current head to be head's next node (the new head)
 	free(temp);				//deallocate the current head from the heap 
 	return true;
 }
@@ -79,11 +85,14 @@ static inline int
 ll_contains(struct linked_list *ll, int value)
 {
 	int pos = 1;
+	struct Node* temp = NULL;
 
-	while (ll != NULL) {
-		if (ll->val == (double) value) 	//first check to see if the current node val is the one we are looking for
+	if (ll != NULL)
+		temp = ll->head;
+	while (temp != NULL) {
+		if (temp->val == value) 	//first check to see if the current node val is the one we are looking for
 			return pos;		//if it is then return it's position
-		ll = ll->next;			//otherwise move to the next node to check there 
+		temp = temp->next;		//otherwise move to the next node to check there 
 		pos++;				//update the respective position as well
 	}
 	return 0;
--- fall2021/cmpe142/assignment_02/your_name/main.c
+++ fall2021/cmpe142/assignment_02/your_name/main.c
@@ -7,25 +7,29 @@ int main(void)
 {
 	struct linked_list* list = ll_create();	
 	
-	ll_add(&list, 12);
+	
+	ll_add(list, 12);
 	ll_add(list, 13);
 	ll_print(list);
-
+	printf("size of list: %d\n", ll_length(list));
+	printf("pos of 12: %d\n", ll_contains(list, 12));
 	ll_remove_first(list);
+	ll_print(list);
 	ll_remove_first(list);
-	ll_print(list);		
+	printf("list destroyed: %d\n", ll_destroy(list));
 	return 0;
 }
 
 void ll_print(struct linked_list* ll) {
 	int pos = 1;
-	
+	struct Node* temp = ll->head;	
 	printf("printing Linked List\n");
-	if (ll == NULL) 
+	if (temp == NULL) 
 		printf("Linked List is empty, nothing to print!\n");
-	while (ll != NULL) {
-		printf("pos %d: %d\n", pos, (int) ll->val);
-		ll = ll->next;
+	while (temp != NULL) {
+		printf("pos %d: %d\n", pos, temp->val);
+		temp = temp->next;
 		pos++;
 	}
 }
+
